30c53c52c757b1ba0dc46bbaed65b5dfea4ac6aa
ryanlavu <ryanvu@ucsb.edu>
2023-05-01 17:09:34 -0700

    Added more for the client

--- v4_client.py
+++ v4_client.py
@@ -42,6 +42,12 @@ def get_user_input():
 			stdout.flush() # imported from sys library
 			# exit program with status 0
 			_exit(0) # imported from os library
+		if user_input_list[0] == "connect":
+			#Manual input to connect to all the other clients
+			#Outgoing connection to other clients
+			for i in range(len(list_pid)):
+				out_sock_list[i].connect((SERVER_IP, SERVER_PORT + ID_int))
+
 		else:
 			user_input_list.append("P" + str(ID))
 			input_string = " ".join(user_input_list)
@@ -80,6 +86,32 @@ def handle_msg(data):
 	# echo message to console
 	print(data)
 
+def respond(conn, addr):
+	#print(f"accepted connection from port {addr[1]}", flush=True)
+
+	# infinite loop to keep waiting to receive new data from this client
+	while True:
+		try:
+			# wait to receive new data, 1024 is receive buffer size
+			data = conn.recv(1024)
+		# handle exception in case something happened to connection
+		# but it's not properly closed
+		except:
+			print(f"exception in receiving from {addr[1]}", flush=True)
+			break
+			
+		# if client's socket closed, it will signal closing without any data
+		if not data:
+			# close own socket to client since other end is closed
+			conn.close()
+			print(f"connection closed from {addr[1]}", flush=True)
+			break
+
+		# spawn a new thread to handle message 
+		# so simulated network delay and message handling don't block receive
+		threading.Thread(target=handle_msg, args=(data, addr)).start()
+
+
 if __name__ == "__main__":
 	sleep(1) # imported from time library
 	# specify server's socket address so client can connect to it
@@ -101,12 +133,11 @@ if __name__ == "__main__":
 
 	#Need to connect to all other clients
 	#Create two more connecting sockets (for the other clients)
+	#!!!! I dont know if this is needed !!!!
 	out_sock_2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	out_sock_3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	out_sock_list = [out_sock_2, out_sock_3]
 
-	for i in range(len(list_pid)):
-		out_sock_list[i].connect((SERVER_IP, SERVER_PORT + ID_int))
 
 	#Need to set up pair of <Local Time, pid> after connecting to all other clients
 	#Start clock off at time 0
@@ -120,6 +151,28 @@ if __name__ == "__main__":
 	# spawn new thread to keep waiting for user inputs
 	# so user input and socket receive do not block each other
 	threading.Thread(target=get_user_input, daemon=False).start()
+
+	out_socks = []
+	#Need to set up listening socket
+	in_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	in_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	in_sock.bind((SERVER_IP, SERVER_PORT + ID_int))
+	in_sock.listen()
+
+	# loop to keep accepting new connections, keep looping until connected to the two other clients
+	while len(out_socks) != 2:
+		try:
+			# wait to accept any incoming connections
+			# conn: socket object used to send to and receive from connection
+			# addr: (IP, port) of connection 
+			conn, addr = in_sock.accept()
+		except:
+			print("exception in accept", flush=True)
+			break
+		# add connection to array to send data through it later
+		out_socks.append((conn, addr))
+		# spawn new thread for responding to each connection
+		threading.Thread(target=respond, args=(conn, addr)).start()
 
 	# infinite loop to keep waiting to receive new data from server
 	while True:
